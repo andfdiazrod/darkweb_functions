d68fa69f5a1298a4b816cf3fd779507dbf8e19d8
jjovalle1999 <56848248+jjovalle1999@users.noreply.github.com>
2019-11-16 10:45:39 -0500

    Update outliers.R

--- functions/outliers.R
+++ functions/outliers.R
@@ -1,3 +1,28 @@
+
+library(dplyr)
+
+df<-read.csv("bases_transformadas/info_total.csv", stringsAsFactors = F)
+outliers<-function(df)
+{
+  
+  df<-df %>% mutate(no_weight=ifelse(is.na(weight_in_grams),1,0))
+  df<- df %>% mutate(outlier_price=ifelse(is.finite(price_in_bit) & price_in_bit>=10, yes = 1,no = 0))
+  df<- df %>% mutate(outlier_price_high=ifelse(is.finite(price_in_bit) & price_in_bit>=10, yes = 1,no = 0))
+  df<- df %>% mutate(outlier_price_low=ifelse(is.finite(price_in_bit) & price_in_bit<=0.01, yes = 1,no = 0))
+  
+  return(df)
+}
+
+df<-outliers(df)
+
+temp<-df %>% filter(no_weight==1 | outlier_price_high==1 | outlier_price_low==1)
+temp_a<-df %>% filter(no_weight==1 )
+summ_a<-listings_summary(temp_a)
+summ_b<-listings_summary(temp_b)
+sum_c<-listings_summary(temp_c)
+temp_b<-df %>% filter( outlier_price_high==1 )
+temp_c<-df %>% filter( outlier_price_low==1)
+
 if(FALSE){
 x=read.csv('C:/Users/Juan Jose/Dropbox/Deepweb/salidas_1_11/listing_summary_df_cocaine.csv')
 
