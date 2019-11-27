c96754d23642cabd4e154b2a6583a996326f7fb7
jjovalle1999 <56848248+jjovalle1999@users.noreply.github.com>
2019-11-27 15:12:15 -0500

    Update price_per_gram.R

--- price_per_gram.R
+++ price_per_gram.R
@@ -4,9 +4,9 @@ price_per_gram<-function(df)
 {
   
   
-   price_per_gram_webpage<-df %>% group_by(day_format, webpage) %>% summarise(market_median_price=median(price_in_bit, na.rm = T))
-   price_per_gram_webpage<-price_per_gram %>% spread(webpage,value = market_median_price)
-   
+  price_per_gram_webpage<-df %>% group_by(day_format, webpage) %>% summarise(market_median_price=median(price_in_bit, na.rm = T))
+  price_per_gram_webpage<-price_per_gram %>% spread(webpage,value = market_median_price)
+  
   df$purity_group<-cut(df$purity,c(50,60,70,80,90,100))
   price_per_gram_purity<-df %>% group_by(day_format, purity_group) %>% summarise(purity_group_median_price=median(price_in_bit, na.rm=T))
   price_per_gram_purity<-price_per_gram_purity %>% spread(purity_group,purity_group_median_price)
@@ -21,7 +21,7 @@ price_per_gram<-function(df)
   a<-1
   for(pais in colnames(df)[startsWith(colnames(df), prefix = "country_list.")])
   {
-  
+    
     print(pais)
     temp<- df[df[,pais]==1,]
     temp <- temp %>% group_by(day_format) %>% summarise(median_price__per_gram_shipping_from= median(price_in_bit, na.rm = T),
@@ -32,7 +32,7 @@ price_per_gram<-function(df)
   
   
   price_per_gram_list<-list(price_per_gram_webpage, price_per_gram_purity, price_per_gram_country_origin, price_per_gram_shipping_from)
-
+  
   return(price_per_gram_list)
-  }
+}
 
